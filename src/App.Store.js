858920bbc1ae2f1532c2b19eca600955ef4627a3
Ryan Choi <choi.link227@gmail.com>
2020-12-13 21:19:43 -0500

    resolve the blank page issue on safari

--- src/App.Store.js
+++ src/App.Store.js
@@ -6,7 +6,9 @@ import { LinkSwapLPToken, ERC20, StakingRewards } from '@util/contracts';
 import { Networks } from '@app/App.Config';
 
 const ethereum = window.ethereum;
-ethereum.autoRefreshOnNetworkChange = false;
+if (ethereum) {
+  ethereum.autoRefreshOnNetworkChange = false;
+}
 const web3 = new Web3(ethereum);
 
 export const AcountStatus = {
@@ -30,7 +32,7 @@ const initialState = {
 const hydration = {
   // fetch network || not available? error
   network: async ({ state, set, hydrate }) => {
-    const chainId = await web3.eth.getChainId();
+    const chainId = ethereum && (await web3.eth.getChainId());
     const network = Networks[chainId];
 
     // no network or unavailable
@@ -51,7 +53,9 @@ const hydration = {
   },
 
   positions: async ({ state, hydrate }) => {
-    Object.values(state?.pool || {}).forEach((pool) => hydrate('pool.userPosition', pool.address));
+    Object.values(state?.pool || {}).forEach((pool) =>
+      hydrate('pool.userPosition', pool.address)
+    );
   },
 
   pool: {
@@ -72,8 +76,14 @@ const hydration = {
       };
 
       // contracts
-      fields.contracts.pool = new web3.eth.Contract(LinkSwapLPToken, poolAddress);
-      fields.contracts.stakingRewards = new web3.eth.Contract(StakingRewards, pool.rewardsAddress);
+      fields.contracts.pool = new web3.eth.Contract(
+        LinkSwapLPToken,
+        poolAddress
+      );
+      fields.contracts.stakingRewards = new web3.eth.Contract(
+        StakingRewards,
+        pool.rewardsAddress
+      );
 
       if (pool.oldRewardsAddress) {
         fields.contracts.oldStakingRewards = new web3.eth.Contract(
@@ -82,20 +92,38 @@ const hydration = {
         );
       }
 
-      fields.periodFinish = await fields.contracts.stakingRewards.methods.periodFinish().call();
-      fields.totalSupply = await fields.contracts.stakingRewards.methods.totalSupply().call();
+      fields.periodFinish = await fields.contracts.stakingRewards.methods
+        .periodFinish()
+        .call();
+      fields.totalSupply = await fields.contracts.stakingRewards.methods
+        .totalSupply()
+        .call();
 
       // token addresses
-      fields.token0.address = await fields.contracts.pool.methods.token0().call();
-      fields.token1.address = await fields.contracts.pool.methods.token1().call();
+      fields.token0.address = await fields.contracts.pool.methods
+        .token0()
+        .call();
+      fields.token1.address = await fields.contracts.pool.methods
+        .token1()
+        .call();
 
       // token contracts
-      fields.token0.contract = new web3.eth.Contract(ERC20, fields.token0.address);
-      fields.token1.contract = new web3.eth.Contract(ERC20, fields.token1.address);
+      fields.token0.contract = new web3.eth.Contract(
+        ERC20,
+        fields.token0.address
+      );
+      fields.token1.contract = new web3.eth.Contract(
+        ERC20,
+        fields.token1.address
+      );
 
       // token symbols
-      fields.token0.symbol = await fields.token0.contract.methods.symbol().call();
-      fields.token1.symbol = await fields.token1.contract.methods.symbol().call();
+      fields.token0.symbol = await fields.token0.contract.methods
+        .symbol()
+        .call();
+      fields.token1.symbol = await fields.token1.contract.methods
+        .symbol()
+        .call();
 
       // token names
       fields.token0.name = await fields.token0.contract.methods.name().call();
@@ -105,22 +133,30 @@ const hydration = {
       fields.reward.yfl.address = await fields.contracts.stakingRewards.methods
         .rewardTokens(0)
         .call();
-      fields.reward.yfl.rate = await fields.contracts.stakingRewards.methods.rewardRate(0).call();
+      fields.reward.yfl.rate = await fields.contracts.stakingRewards.methods
+        .rewardRate(0)
+        .call();
 
       // check for ERT rewards
       try {
-        const ert_address = await fields.contracts.stakingRewards.methods.rewardTokens(1).call();
-        const ert_rate = await fields.contracts.stakingRewards.methods.rewardRate(1).call();
+        const ert_address = await fields.contracts.stakingRewards.methods
+          .rewardTokens(1)
+          .call();
+        const ert_rate = await fields.contracts.stakingRewards.methods
+          .rewardRate(1)
+          .call();
 
-         if (ert_address !== '0x0000000000000000000000000000000000000000') {
-           fields.reward.ert = {
-             address: ert_address,
-             rate: ert_rate,
-           };
-         }
+        if (ert_address !== '0x0000000000000000000000000000000000000000') {
+          fields.reward.ert = {
+            address: ert_address,
+            rate: ert_rate,
+          };
+        }
       } catch (e) {}
 
-      update(`pool.${poolAddress}`, fields, () => hydrate('pool.values', poolAddress));
+      update(`pool.${poolAddress}`, fields, () =>
+        hydrate('pool.values', poolAddress)
+      );
     },
 
     // hydrate pool values
@@ -142,14 +178,26 @@ const hydration = {
       fields.totalSupply = await PoolContract.methods.totalSupply().call();
       fields.staked = await StakingRewardsContract.methods.totalSupply().call();
 
-      const token0Contract = new web3.eth.Contract(ERC20, fields.token0.address);
-      const token1Contract = new web3.eth.Contract(ERC20, fields.token1.address);
+      const token0Contract = new web3.eth.Contract(
+        ERC20,
+        fields.token0.address
+      );
+      const token1Contract = new web3.eth.Contract(
+        ERC20,
+        fields.token1.address
+      );
 
       // hydrate token balances
-      fields.token0.balance = await token0Contract.methods.balanceOf(address).call();
-      fields.token1.balance = await token1Contract.methods.balanceOf(address).call();
+      fields.token0.balance = await token0Contract.methods
+        .balanceOf(address)
+        .call();
+      fields.token1.balance = await token1Contract.methods
+        .balanceOf(address)
+        .call();
 
-      update(`pool.${poolAddress}`, fields, () => hydrate('pool.userPosition', poolAddress));
+      update(`pool.${poolAddress}`, fields, () =>
+        hydrate('pool.userPosition', poolAddress)
+      );
     },
 
     // hydrate pool user position
@@ -175,13 +223,21 @@ const hydration = {
           .balanceOf(accountAddress)
           .call();
       }
-      position.balance = await PoolContract.methods.balanceOf(accountAddress).call();
-      position.staked = await StakingRewardsContract.methods.balanceOf(accountAddress).call();
-      position.reward.yfl = await StakingRewardsContract.methods.earned(accountAddress, 0).call();
+      position.balance = await PoolContract.methods
+        .balanceOf(accountAddress)
+        .call();
+      position.staked = await StakingRewardsContract.methods
+        .balanceOf(accountAddress)
+        .call();
+      position.reward.yfl = await StakingRewardsContract.methods
+        .earned(accountAddress, 0)
+        .call();
 
       try {
         if (reward.ert) {
-          const ert_rewards = await StakingRewardsContract.methods.earned(accountAddress, 1).call();
+          const ert_rewards = await StakingRewardsContract.methods
+            .earned(accountAddress, 1)
+            .call();
           position.reward.ert = ert_rewards;
         }
       } catch (e) {}
@@ -196,7 +252,7 @@ const triggers = {
     connect: async ({ state, set, trigger }) => {
       if (!web3 || state.network?.enabled !== true) {
         set('account.status', AcountStatus.ERROR);
-        Notification.error('No web3!...');
+        Notification.error('No web3!');
         return;
       }
 
@@ -227,7 +283,12 @@ const triggers = {
     fetchEthBalance: ({ state, set }) => {
       web3.eth
         .getBalance(state.account?.address)
-        .then((wei) => set('account.balance.eth', parseFloat(web3.utils.fromWei(wei)).toFixed(2)));
+        .then((wei) =>
+          set(
+            'account.balance.eth',
+            parseFloat(web3.utils.fromWei(wei)).toFixed(2)
+          )
+        );
     },
     fetchTokenBalance: async ({ state, set }, address) => {
       const contract = new web3.eth.Contract(LinkSwapLPToken, address);
@@ -235,7 +296,10 @@ const triggers = {
         .balanceOf(state.account?.address)
         .call()
         .then((wei) => {
-          set(`account.balance.${address}`, parseFloat(web3.utils.fromWei(wei)).toFixed(2));
+          set(
+            `account.balance.${address}`,
+            parseFloat(web3.utils.fromWei(wei)).toFixed(2)
+          );
         })
         .catch((e) => console.log(e));
     },
@@ -247,7 +311,10 @@ const subscribers = {
     switch (status) {
       case AcountStatus.CONNECTED:
         trigger('account.fetchEthBalance');
-        trigger('account.fetchTokenBalance', state.network?.params?.tokenAddress);
+        trigger(
+          'account.fetchTokenBalance',
+          state.network?.params?.tokenAddress
+        );
         hydrate('positions');
         break;
       case AcountStatus.DISCONNECTED:
@@ -262,8 +329,10 @@ const subscribers = {
 
 const init = ({ hydrate, trigger }) => {
   hydrate('network', null, ({ state, set }) => {
-    window.ethereum.on('chainChanged', () => hydrate('network'));
-    window.ethereum.on('accountsChanged', () => trigger('account.change'));
+    window.ethereum &&
+      window.ethereum.on('chainChanged', () => hydrate('network'));
+    window.ethereum &&
+      window.ethereum.on('accountsChanged', () => trigger('account.change'));
     hydrate('pools');
   });
 };
